2f681eece466ab0b1cba283b8ba76d11401fa5bc
rbetik12 <mr.prikota@gmail.com>
2022-04-02 02:11:55 +0300

    Updated texture API

--- Engine/Source/Core/RHI/Public/Texture.hpp
+++ Engine/Source/Core/RHI/Public/Texture.hpp
@@ -5,23 +5,37 @@
 
 namespace RightEngine
 {
+    struct TextureSpecification
+    {
+        int width{ 0 };
+        int height{ 0 };
+        int componentAmount{ 0 };
+    };
+
     class Texture
     {
     public:
         explicit Texture(const std::string& path);
+        explicit Texture(const TextureSpecification& specification, const void* data);
         ~Texture();
 
         void Bind(uint32_t slot = 0) const;
         void UnBind() const;
 
         inline int GetWidth() const
-        { return width; }
+        { return specification.width; }
 
         inline int GetHeight() const
-        { return height; }
+        { return specification.height; }
+
+        inline uint32_t GetId() const
+        { return id; }
 
     private:
-        int width, height, componentAmount;
+        TextureSpecification specification;
         uint32_t id;
+
+    private:
+        void Generate(const void* data);
     };
 }
--- Engine/Source/Core/RHI/Texture.cpp
+++ Engine/Source/Core/RHI/Texture.cpp
@@ -1,6 +1,7 @@
 #include "Texture.hpp"
 #include "Logger.hpp"
 #include "Path.hpp"
+#include "Assert.hpp"
 #include <stb_image.h>
 #include <glad/glad.h>
 
@@ -8,31 +9,26 @@
 RightEngine::Texture::Texture(const std::string& path)
 {
     //stbi_set_flip_vertically_on_load(1);
-    unsigned char* localBuffer = stbi_load(Path::ConvertEnginePathToOSPath(path).c_str(), &width, &height, &componentAmount, 0);
-    if (localBuffer) {
-        GLenum format = GL_RGBA;
-        if (componentAmount == 1)
-            format = GL_RED;
-        else if (componentAmount == 3)
-            format = GL_RGB;
-
-        glGenTextures(1, &id);
-        glBindTexture(GL_TEXTURE_2D, id);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, localBuffer);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-        UnBind();
+    unsigned char* localBuffer = stbi_load(Path::ConvertEnginePathToOSPath(path).c_str(),
+                                           &specification.width,
+                                           &specification.height,
+                                           &specification.componentAmount,
+                                           0);
+    if (localBuffer)
+    {
+        Generate(localBuffer);
         R_CORE_INFO("Load texture at path {0} successfully!", path);
     }
-    else {
+    else
+    {
         R_CORE_ERROR("Failed to load texture at path: {0}", path);
     }
     stbi_image_free(localBuffer);
+}
+
+RightEngine::Texture::Texture(const TextureSpecification& specification, const void* data) : specification(specification)
+{
+    Generate(data);
 }
 
 void RightEngine::Texture::Bind(uint32_t slot) const
@@ -49,4 +45,34 @@ void RightEngine::Texture::UnBind() const
 RightEngine::Texture::~Texture()
 {
     glDeleteTextures(1, &id);
+}
+
+void RightEngine::Texture::Generate(const void* data)
+{
+    R_CORE_ASSERT(data && specification.width > 0 && specification.height > 0, "Texture data is null!");
+    GLenum format = GL_RGBA;
+    if (specification.componentAmount == 1)
+    {
+        format = GL_RED;
+    }
+    else if (specification.componentAmount == 3)
+    {
+        format = GL_RGB;
+    }
+    else if (specification.componentAmount != 4)
+    {
+        R_CORE_ASSERT(false, "Unsupported components amount");
+    }
+
+    glGenTextures(1, &id);
+    glBindTexture(GL_TEXTURE_2D, id);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, format, specification.width, specification.height, 0, format, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    UnBind();
 }
